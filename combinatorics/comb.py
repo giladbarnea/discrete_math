d92e23c22ebda51e5f62698232976ee31564202d
Gilad Barnea <giladbrn@gmail.com>
2020-11-20 13:08:36 +0200

    comb.py fac(a,b=None) doctests pass

--- combinatorics/comb.py
+++ combinatorics/comb.py
@@ -6,7 +6,9 @@ from pprint import pprint
 import re
 from copy import deepcopy
 from collections import defaultdict
+from math import factorial
 
+import doctest
 
 _print = deepcopy(print)
 def newprint(*args, **kwargs):
@@ -39,14 +41,39 @@ def fac_str(start, stop=1):
     else:
         return f"{start}·…·{stop}"
     
-def fac(start, stop=1):
+def fac(a, b=None):
+    """
+    Inclusive both sides.
     
-    mult = start if start else 1
-    for n in reversed(range(stop, start)):
-        if n == 0:
-            n = 1
+    >>> all(fac(n) == factorial(n) == fac(0, n) == fac(1, n) for n in range(7))
+    True
+    
+    >>> [f'2→{n}={fac(2, n)}' for n in range(7)]
+    ['2→0=2', '2→1=2', '2→2=2', '2→3=6', '2→4=24', '2→5=120', '2→6=720']
+    
+    >>> [f'{n}→2={fac(n, 2)}' for n in range(7)]
+    ['0→2=2', '1→2=2', '2→2=2', '3→2=6', '4→2=24', '5→2=120', '6→2=720']
+    
+    >>> [f'3→{n}={fac(3, n)}' for n in range(7)]
+    ['3→0=6', '3→1=6', '3→2=6', '3→3=3', '3→4=12', '3→5=60', '3→6=360']
+    
+    >>> [f'{n}→3={fac(n, 3)}' for n in range(7)]
+    ['0→3=6', '1→3=6', '2→3=6', '3→3=3', '4→3=12', '5→3=60', '6→3=360']
+    
+    """
+    
+    if b is None:
+        return factorial(a)
+    high = max(a,b)+1
+    low = min(a,b) or 1
+    mult = 1
+    # mult = start if start!=0 else 1
+    for n in range(low, high):
         mult = mult * n
     return mult
+
+
+
 
 def bold_num(s):
     bold = re.sub(r"\d+\b", lambda match: f"\x1b[1m{match.group()}\x1b[0m", s)
@@ -120,6 +147,45 @@ def P_spam(*, byres=False):
             print(f"{res}:\t{', '.join(how)}")
      
 
+    
+    
+    
+    
+def Q(n, k, *, explain=True, example=False):
+    _Pn = n+k-1
+    if isinstance(n, int):
+        rv = len(list(permutations(range(_Pn), k)))
+        kia = ascii_uppercase[:k]
+        vlines = "|" * (n-1)
+        if explain:
+            if kia:
+                orderstr = f"• Order matters, so '{kia}{vlines}' and '{kia[1:]}{kia[0]}{vlines}' are both counted"
+            else:
+                orderstr="• Order matters, so '△◯|' and '◯△|' are both counted"
+            print(
+                f"Q({n},{k})",
+                f"{rv} ways to organize {k} distinct objects into {n} bins",
+                orderstr,
+                "• No repetitions allowed",
+                sep="\n\t",
+            )
+        
+        if example:
+            letters = ascii_uppercase[:k] + vlines
+            [print(f"{i}:\t", *comb, sep="") for i, comb in enumerate(set(permutations(letters, len(letters))), 1)]
+
+            print()
+
+    else:
+        rv = list(permutations(n, k))
+    return rv
+    
+
+    
+    
+    
+    
+    
 def Pnk(n, *nks):
     if isinstance(n, int):
         denominator = 1
@@ -153,7 +219,16 @@ def Pnk_spam(*, byres=False, ofnum=None):
     elif byres:
         for res, how in sorted(results.items()):
             print(f"{res}:\t{', '.join(how)}")
-            
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
             
 def nk(n,k, *, example=False):
     if isinstance(n, int):
@@ -239,10 +314,10 @@ def C_spam(*, byres=False):
         for res, how in sorted(results.items()):
             print(f"{res}:\t{', '.join(how)}")
         
-def C_id(n, k, v):
-    _k = n - k
+def C_id(n, k, v)->str:
     v1 = f"C({n},{k}) vI  = P({n},{k})/P({k}) = ({fac_str(n, n-k+1)})/({fac_str(k)}) = {fac(n, n-k+1)}/{fac(k)}"
     
+    _k = n - k
     v2 = f"C({n},{_k}) vII = P({n},{_k})/P({_k}) = ({fac_str(n, n-_k+1)})/({fac_str(_k)}) = {fac(n, n-_k+1)}/{fac(_k)}"
     
     if "II" in v:
@@ -296,11 +371,11 @@ def D(n, k, *, explain=True, example=False, identities=False):
             C(_Cn1, _Dk2, explain=explain, example=example, identities=False)
         if example:
             _print(f"\n\x1b[1mD({n},{k}) Examples\x1b[0m")
-            letters = "○" * k + "|" * (n - 1)
+            letters = "◯" * k + "|" * (n - 1)
             [print(f"{i}:\t", *comb, sep="") for i, comb in enumerate(set(permutations(letters, len(letters))), 1)]
             if identities:
                 _print(f"\n\x1b[1mD({_Dn2},{_Dk2}) Examples\x1b[0m")
-                letters = "○" * _Dk2 + "|" * (_Dn2 - 1)
+                letters = "◯" * _Dk2 + "|" * (_Dn2 - 1)
                 [print(f"{i}:\t", *comb, sep="") for i, comb in enumerate(set(permutations(letters, len(letters))), 1)]
 
             print()
@@ -335,3 +410,11 @@ def C2D(n, k):
     C2D(7,2) prints D(6,2), D(6,5)
     """
     D(n - k + 1, k, identities=True)
+    
+    
+    
+# doctest.testfile(__file__, module_relative=False, globs=globals(), optionflags=doctest.ELLIPSIS)
+
+# for n in range(7):
+#     assert fac(n) == factorial(n) == fac(0, n) == fac(1, n)
+#     assert fac(2, n) == fac(n)
